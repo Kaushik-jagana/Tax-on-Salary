2c149219e0e1c8568053431e25035768ed2cb5a7
Kaushik-jagana <jaganakaushik@gmail.com>
2023-01-11 10:36:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
-    console.log(100)
+    // console.log(100)
     res.sendFile(path.join(__dirname, '/index.html'));    
 });
 
@@ -13,22 +13,27 @@ app.post('/', (req, res) => {
     // console.log(100)
     let val = req.body;
     let ans= sol(val["LPA"]);
+    console.log(ans);
     // res.send(ans);
 
 });
 
 function sol(amt){
     let ans=0;
-    if(1000000>=amt>250000){
-        ans+= (Math.min(amt-250000,7500000))/10; 
+    // let amt=Number(amount);
+    if(amt>250000){
+        ans+= (Math.min(amt-250000,750000))/10; 
+        console.log(ans);
     }
-    if(1000000<amt<=1500000){
+    if(1000000<amt ){
         ans+=(Math.min(amt-1000000,500000))*(15/100);
         console.log(ans);
     }
     if(amt>1500000){
         ans+=(amt-1500000)(30/100);
     }
+    console.log("ans=")
+    console.log(ans);
     return amt-ans;   
 }
 
